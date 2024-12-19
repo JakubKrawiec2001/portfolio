ad3982a10a6776c17c736da22ccf43d22bc37717
Jakub Krawiec <kubakrawieckk04@gmail.com>
2024-12-19 11:52:01 +0100

    added new assets

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import { navLinks } from "../constansts";
+import square2 from "/icons/squares-2.svg";
 
 const Nav = () => {
   return (
     <header className="fixed inset-0 h-16 py-10 bg-[#f9f9f9] border-b-2 border-custom-black flex items-center justify-between z-50 mx-12">
       <div className="flex items-center gap-12">
+        <img src={square2} alt="square" className="" />
         {navLinks.map((link) => {
           return (
             <a
